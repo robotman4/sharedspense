92cf31eb3eea3ec556c37a3522c6f8846a888620
felipgit <kim@felip.se>
2024-06-09 13:43:43 +0200

    DOMContentLoaded

--- src/frontend/js/client.js
+++ src/frontend/js/client.js
@@ -1,6 +1,8 @@
-document.getElementById('linkLogout').addEventListener('click', function(event){
-    event.preventDefault();
-    logout();
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('linkLogout').addEventListener('click', function(event){
+        event.preventDefault();
+        logout();
+    });
 });
 
 // TODO: make buttons highlighted dynamically based on selection
--- src/frontend/js/login.js
+++ src/frontend/js/login.js
@@ -1,10 +1,12 @@
-document.getElementById('buttonLogin').addEventListener('click', function() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    login(username, password);
-});
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('buttonLogin').addEventListener('click', function() {
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+        login(username, password);
+    });
 
-document.getElementById('dismissError').addEventListener('click', function() {
-    const progress = document.getElementById('dialogProgress');
-    progress.close();
+    document.getElementById('dismissError').addEventListener('click', function() {
+        const progress = document.getElementById('dialogProgress');
+        progress.close();
+    });
 });
