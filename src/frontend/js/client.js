766aed0224261e090fbec1a82e36d3aa1e9b4d08
felipgit <kim@felip.se>
2024-06-10 23:06:49 +0200

    Fix dynamic menu highlight and load specific content for specific window location

--- src/frontend/js/client.js
+++ src/frontend/js/client.js
@@ -1,22 +1,42 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('linkLogout').addEventListener('click', function(event){
+    const currentPath = window.location.pathname;
+
+    document.getElementById('linkMenuLogout').addEventListener('click', function(event){
         event.preventDefault();
         logout();
     });
 
-    document.getElementById('buttonAddExpense').addEventListener('click', function(event) {
-        event.preventDefault();
-        addExpense();
-    });
+    // Check if the current URL matches '/client/current'
+    if (currentPath === '/client/current') {
+        // Call your function
+        fetchUnapprovedExpenses();
 
-    document.getElementById('buttonDeleteExpense').addEventListener('click', function(event) {
-        event.preventDefault();
-        const expenseId = this.dataset.expenseId;
-        deleteExpense(expenseId);
+        document.getElementById('buttonAddExpense').addEventListener('click', function(event) {
+            event.preventDefault();
+            addExpense();
+        });
+
+        document.getElementById('buttonDeleteExpense').addEventListener('click', function(event) {
+            event.preventDefault();
+            const expenseId = this.dataset.expenseId;
+            deleteExpense(expenseId);
+        });
+    }
+
+
+    const links = document.querySelectorAll('a');
+
+    // Loop through all links
+    links.forEach(link => {
+        // Get the href attribute of the link
+        const href = link.getAttribute('href');
+        
+        // Check if the href matches the current path
+        if (href === currentPath) {
+            // Get the <i> element inside the link
+            const icon = link.querySelector('i');
+            // Add the 'fill' class to the <i> element
+            icon.classList.add('fill');
+        }
     });
 });
-
-//TODO: run only for current view
-fetchUnapprovedExpenses();
-
-// TODO: make buttons highlighted dynamically based on selection
